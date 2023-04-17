1331e16c286e65098dca0100c28faa45a341230a
Alex <page.alexanderjohn@gmail.com>
2023-04-17 17:47:46 -0400

    Small update

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=50)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    info = models.CharField(max_length=500)
+    imageURL = models.URLField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='user')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name='category')
